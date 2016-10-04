abe970cbfdf090ffaf968e2657f83630bf702500
Mahdavi <foroozan.mahdavi@euromoneyplc.com>
2016-10-04 13:19:32 +0100

    f1

---
+++ Katas.Tests/CheckerTests.cs
@@ -0,0 +1,23 @@
+﻿using Katas.Model;
+using NUnit.Framework;
+
+namespace Katas.Tests
+{
+    [TestFixture]
+    public class CheckerTests
+    {
+        [Test]
+        public void CanMoveOneSteps()
+        {
+            var board = new Board();
+            var piece = board.AddPiece(0, 0);
+
+            piece.MoveUp();
+
+            Assert.That(piece.Column, Is.EqualTo(1));
+            Assert.That(piece.Row, Is.EqualTo(1));
+
+        }
+
+    }
+}
---
+++ Katas/Model/Game.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Katas.Model
+{
+    public class Board
+    {
+        public Piece AddPiece(int i, int i1)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class Piece
+    {
+        public void MoveUp()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Column()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Row()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
