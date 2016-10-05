7e7b50ca320401246cf826556d55d54b146cecc9
Rob Cooper <robcthegeek.public@gmail.com>
2016-10-05 18:23:26 +0100

    Update Tests to NUnit & (Auto)NSubstitute

---
+++ Katas.Tests/AutoTestFixture.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using Ploeh.AutoFixture;
+using Ploeh.AutoFixture.AutoNSubstitute;
+
+namespace Katas.Tests
+{
+    [TestFixture]
+    public abstract class AutoTestFixture
+    {
+        protected IFixture Fixture { get; private set; }
+
+        [SetUp]
+        public void SetUp()
+        {
+            Fixture = new Fixture()
+                .Customize(new AutoNSubstituteCustomization());
+        }
+    }
+}
