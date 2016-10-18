4f5e8495f386b355489cf4c1fde18d81f11be373
Rob Cooper <robc.the.geek@gmail.com>
2016-10-18 13:46:16 +0100

    Adding BoardRenderer

---
+++ Katas.Tests/Models/BoardRendererTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Katas.Model;
+using NUnit.Framework;
+
+namespace Katas.Tests.Models
+{
+    [TestFixture]
+    public class BoardRendererTests
+    {
+        [Test]
+        public void Ctor_NullBoard_ThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(
+                () => new BoardRenderer(null));
+        }
+
+        [Test]
+        public void Render_ANewBoard_AcceptBoardInConstructor()
+        {
+            var boardRenderer = new BoardRenderer(new Board());
+
+            var expected =
+                "▒▓▒▓▒▓▒▓" + Environment.NewLine +
+                "▓▒▓▒▓▒▓▒" + Environment.NewLine +
+                "▒▓▒▓▒▓▒▓" + Environment.NewLine +
+                "▓▒▓▒▓▒▓▒" + Environment.NewLine +
+                "▒▓▒▓▒▓▒▓" + Environment.NewLine +
+                "▓▒▓▒▓▒▓▒" + Environment.NewLine +
+                "▒▓▒▓▒▓▒▓" + Environment.NewLine +
+                "▓▒▓▒▓▒▓▒";
+
+            var squares = boardRenderer.Render();
+
+            Assert.That(squares, Is.EqualTo(expected));
+        }
+
+    }
+}
---
+++ Katas/Model/BoardRenderer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Katas.Model
+{
+    public class BoardRenderer
+    {
+        private Board _board;
+
+        public BoardRenderer(Board board)
+        {
+            if (board == null) throw new ArgumentNullException(nameof(board));
+
+            _board = board;
+        }
+
+        public string Render()
+        {
+            var oddRow = string.Join("", Enumerable.Range(1, 8).Select(column => column % 2 == 0 ? "▓" : "▒"));
+            var evenRow = string.Join("", Enumerable.Range(1, 8).Select(column => column % 2 == 0 ? "▒" : "▓"));
+
+            return string.Join(
+                Environment.NewLine,
+                Enumerable.Range(1, 8)
+                    .Select(j => j % 2 == 0 ? evenRow : oddRow));
+
+        }
+    }
+}
