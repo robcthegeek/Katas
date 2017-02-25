61d8fb6722855d58b2b54a319b582b4dffdcade1
Rob Cooper <robc.the.geek@gmail.com>
2017-02-25 18:03:25 +0000

    Hacking on Models etc.

---
+++ Katas.Tests/GameTests.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+
+namespace Katas.Tests
+{
+    public class GameTests
+    {
+        // TODO (RC): Build Picture of "Game" (pieces in play)..
+
+        // TODO (RC): Ensure First Turn < 100ms
+
+        // TODO (RC): Ensure Other Turns < 50ms
+
+        [Test]
+        public void NextMove_OnlyTwoFactoriesAndPlayerHasMoreTroops_ReturnsMoveEnemyFactory()
+        {
+            // 2 Factories, 1 Link, 2 Different Players
+            var f1 = new Factory(1, Owner.Player, 10, 5);
+            var f2 = new Factory(2, Owner.Enemy, 1, 5);
+
+            var game = new Game(2, 1, new []
+            {
+                new FactoryLink(1, 2, 5),
+            });
+
+            game.SetState(new [] { f1, f2 });
+
+            var move = (MoveAction)game.NextMove();
+
+            Assert.That(move.Source, Is.EqualTo(1));
+            Assert.That(move.Destination, Is.EqualTo(2));
+            Assert.That(move.CyborgCount, Is.EqualTo(10));
+        }
+    }
+}
---
+++ Katas/Game.cs
@@ -0,0 +1,158 @@
+﻿using System.Collections.Generic;
+
+namespace Katas
+{
+    public class Game
+    {
+        public int FactoryCount { get; private set; }
+        public int LinkCount { get; private set; }
+        public IEnumerable<FactoryLink> Links { get; private set; }
+
+        public Game(int factoryCount, int linkCount, IEnumerable<FactoryLink> links)
+        {
+            FactoryCount = factoryCount;
+            LinkCount = linkCount;
+            Links = links;
+        }
+
+        public void SetState(IEnumerable<Entity> entities)
+        {
+
+        }
+
+        public Action NextMove()
+        {
+
+            // TODO (RC): Strategy? KILL vs. CAPTURE?
+            // <= Troops than enemy? CAPTURE
+            // > Troops than enemy? KILL
+
+            // TODO (RC): Determine Possible Options for moves (Player Owned > Links > Bases)
+            // Each Option will have:
+            // Source / Target Factories
+            // Turns Required
+            // Troop Count on Arrival (Turns Required * Target Production)
+            // Expected Losses (Availability - (Troops on Arrival + Troops Inbound)
+
+
+            // TODO (RC): Find Quickest Move that Captures Most Production w/ Least Losses
+
+            // TODO (RC): Also need to think defense - if there are troops incoming, then we need to weigh up production losses
+
+            return new MoveAction(1, 2, 10);
+        }
+    }
+
+    public abstract class Entity
+    {
+        public static Entity Create(int id, string type, int arg1, int arg2, int arg3, int arg4, int arg5)
+        {
+            if (type == "FACTORY")
+            {
+                return new Factory(id, arg1, arg2, arg3);
+            }
+
+            if (type == "TROOP")
+            {
+                return new Troop(id, arg1, arg2, arg3, arg4, arg5);
+            }
+
+            return null;
+        }
+    }
+
+    public class Troop : Entity
+    {
+        public int Id { get; private set; }
+        public int Owner { get; private set; }
+        public int SourceFactoryId { get; private set; }
+        public int TargetFactoryId { get; private set; }
+        public int Headcount { get; private set; }
+        public int TurnsRemaining { get; private set; }
+
+        public Troop(int id, int owner, int sourceFactoryId, int targetFactoryId, int headcount, int turnsRemaining)
+        {
+            Id = id;
+            Owner = owner;
+            SourceFactoryId = sourceFactoryId;
+            TargetFactoryId = targetFactoryId;
+            Headcount = headcount;
+            TurnsRemaining = turnsRemaining;
+        }
+    }
+    public class Factory : Entity
+    {
+        public int Id { get; private set; }
+        public int Owner { get; private set; }
+        public int Cyborgs { get; private set; }
+        public int Production { get; private set; }
+
+        public Factory(int id, int owner, int cyborgs, int production)
+        {
+            Id = id;
+            Owner = owner;
+            Cyborgs = cyborgs;
+            Production = production;
+        }
+
+        public Factory(int id, Owner owner, int cyborgs, int production) :
+            this (id, (int)owner, cyborgs, production)
+        {
+
+        }
+    }
+
+    public enum Owner
+    {
+        Player = 1,
+        Neutral = 0,
+        Enemy = -1
+    }
+
+    public class FactoryLink
+    {
+        public int Factory1 { get; private set; }
+        public int Factory2 { get; private set; }
+        public int Distance { get; private set; }
+
+        public FactoryLink(int factory1, int factory2, int distance)
+        {
+            Factory1 = factory1;
+            Factory2 = factory2;
+            Distance = distance;
+        }
+    }
+
+    public abstract class Action
+    {
+        public new abstract string ToString();
+    }
+
+    public class WaitAction : Action
+    {
+        public override string ToString()
+        {
+            return "WAIT";
+        }
+    }
+
+    public class MoveAction : Action
+    {
+        public int Source { get; private set; }
+        public int Destination { get; private set; }
+        public int CyborgCount { get; private set; }
+
+
+        public MoveAction(int source, int destination, int cyborgCount)
+        {
+            Source = source;
+            Destination = destination;
+            CyborgCount = cyborgCount;
+        }
+
+        public override string ToString()
+        {
+            return $"MOVE {Source} {Destination} {CyborgCount}";
+        }
+    }
+}
