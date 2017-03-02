0e067f867da5d537917d5e0b2b30652793b7eab0
Rob Cooper <robc.the.geek@gmail.com>
2017-03-02 21:10:14 +0000

    Refactor GameStateTests

--- Katas.Tests/GameStateTests.cs
+++ Katas.Tests/GameStateTests.cs
@@ -9,51 +9,43 @@ namespace Katas.Tests
         [Test]
         public void Factories_TwoFactoriesTwoTroops_ReturnsOnlyFactories()
         {
-            var l1 = new FactoryLink(1, 2, 1);
-            var f1 = new Factory(1, Owner.Player, 5, 3);
-            var f2 = new Factory(2, Owner.Enemy, 5, 3);
-            var t1 = new Troop(3, Owner.Player, 1, 2, 1, 1);
-            var t2 = new Troop(4, Owner.Enemy, 2, 1, 1, 1);
-
-            var state = new GameState(new List<FactoryLink> { l1 });
-            state.AddEntities(f1, f2, t1, t2);
+            GameState state = GameStateBuilder
+                .With
+                .PlayerFactory(1, 5, 3)
+                .EnemyFactory(2, 5, 3)
+                .Link(1, 2, 1)
+                .PlayerTroop(3, 1, 2, 1, 1)
+                .EnemyTroop(4, 1, 2, 1, 1);
 
             var factories = state.Factories;
 
             Assert.That(factories.Count, Is.EqualTo(2));
-            Assert.That(factories, Contains.Item(f1));
-            Assert.That(factories, Contains.Item(f2));
         }
 
         [Test]
         public void Troops_TwoFactoriesTwoTroops_ReturnsOnlyTroops()
         {
-            var l1 = new FactoryLink(1, 2, 1);
-            var f1 = new Factory(1, Owner.Player, 5, 3);
-            var f2 = new Factory(2, Owner.Enemy, 5, 3);
-            var t1 = new Troop(3, Owner.Player, 1, 2, 1, 1);
-            var t2 = new Troop(4, Owner.Enemy, 2, 1, 1, 1);
-
-            var state = new GameState(new List<FactoryLink> { l1 });
-            state.AddEntities(f1, f2, t1, t2);
+            GameState state = GameStateBuilder
+                .With
+                .PlayerFactory(1, 5, 3)
+                .EnemyFactory(2, 5, 3)
+                .Link(1, 2, 1)
+                .PlayerTroop(3, 1, 2, 1, 1)
+                .EnemyTroop(4, 1, 2, 1, 1);
 
             var troops = state.Troops;
 
             Assert.That(troops.Count, Is.EqualTo(2));
-            Assert.That(troops, Contains.Item(t1));
-            Assert.That(troops, Contains.Item(t2));
         }
 
         [Test]
         public void PossibleActions_TwoFactories_ReturnsPossibleWaitMove()
         {
-            // You can always wait :)
-            var l1 = new FactoryLink(1, 2, 1);
-            var f1 = new Factory(1, Owner.Player, 5, 3);
-            var f2 = new Factory(2, Owner.Enemy, 5, 3);
-
-            var state = new GameState(new List<FactoryLink> { l1 });
-            state.AddEntities(f1, f2);
+            GameState state = GameStateBuilder
+                .With
+                .PlayerFactory(1, 5, 3)
+                .EnemyFactory(2, 5, 3)
+                .Link(1, 2, 1);
 
             var actions = state.PossibleActions();
 
@@ -63,32 +55,30 @@ namespace Katas.Tests
         [Test]
         public void PossibleActions_TwoFactories_ReturnsPossibleMoveAction()
         {
-            var l1 = new FactoryLink(1, 2, 1);
-            var f1 = new Factory(1, Owner.Player, 5, 3);
-            var f2 = new Factory(2, Owner.Enemy, 5, 3);
-
-            var state = new GameState(new List<FactoryLink> { l1 });
-            state.AddEntities(f1, f2);
+            GameState state = GameStateBuilder
+                .With
+                .PlayerFactory(1, 5, 3)
+                .EnemyFactory(2, 5, 3)
+                .Link(1, 2, 1);
 
             var actions = state.PossibleActions();
 
             var move = actions.Single(x => x.Action is MoveAction).Action as MoveAction;
-            Assert.That(move.Source, Is.EqualTo(f1.Id));
-            Assert.That(move.Destination, Is.EqualTo(f2.Id));
-            Assert.That(move.CyborgCount, Is.EqualTo(f1.Cyborgs));
+            Assert.That(move.Source, Is.EqualTo(1));
+            Assert.That(move.Destination, Is.EqualTo(2));
+            Assert.That(move.CyborgCount, Is.EqualTo(5));
         }
 
         [Test]
         public void PossibleActions_TwoFactoriesLinkedToPlayer_ReturnsPossibleMoveAction()
         {
-            var l1 = new FactoryLink(1, 2, 1);
-            var l2 = new FactoryLink(1, 3, 1);
-            var f1 = new Factory(1, Owner.Player, 5, 3);
-            var f2 = new Factory(2, Owner.Enemy, 5, 3);
-            var f3 = new Factory(3, Owner.Enemy, 5, 3);
-
-            var state = new GameState(new List<FactoryLink> { l1, l2 });
-            state.AddEntities(f1, f2, f3);
+            GameState state = GameStateBuilder
+                .With
+                .PlayerFactory(1, 5, 3)
+                .EnemyFactory(2, 5, 3)
+                .EnemyFactory(3, 5, 3)
+                .Link(1, 2, 1)
+                .Link(1, 3, 1);
 
             var actions = state.PossibleActions();
 
