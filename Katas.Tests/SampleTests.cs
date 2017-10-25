dc51aec98b73a2c1a65c96d12bbd71f98ce47ffc
Rob Cooper <robc.the.geek@gmail.com>
2017-10-25 04:21:38 +0100

    Update Packages

---
+++ Katas.Tests/SampleTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace Katas.Tests
+{
+    [TestFixture]
+    public class SampleTests
+    {
+        [Test]
+        public void Test_ShouldWork_IsGreen()
+        {
+            Assert.True(true);
+        }
+    }
+}
