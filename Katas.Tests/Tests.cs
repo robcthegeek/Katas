b43cd3b0f1c612cd7a70aa575318cc88eda2a058
Rob Cooper <robc.the.geek@gmail.com>
2018-02-14 06:02:32 +0000

    WIP: WAR!

--- Katas.Tests/Tests.cs
+++ Katas.Tests/Tests.cs
@@ -12,24 +12,37 @@ namespace Katas.Tests
     public class Tests
     {
         [Test]
-        public void AllHorsesTie()
+        public void ThreeCards()
         {
-            var actual = HorseStrengths.DifferenceBetweenClosest(new[] { 2, 2, 2, 2 });
-            Assert.AreEqual(0, actual);
+            var actual = War.Play(new[] { "AD", "KC", "QC" }, new[] { "KH", "QS", "JC" });
+            Assert.AreEqual("1 3", actual);
         }
 
         [Test]
-        public void SimpleCase()
+        public void TwentySixCards()
         {
-            var actual = HorseStrengths.DifferenceBetweenClosest(new[] { 3, 5, 8, 9 });
-            Assert.AreEqual(1, actual);
+            var actual = War.Play(
+                new[] { "5C", "3D", "2C", "7D", "8C", "7S", "5D", "5H", "6D", "5S", "4D", "6H", "6S", "3C", "3S", "7C", "4S", "4H", "7H", "4C", "2H", "6C", "8D", "3H", "2D", "2S" },
+                new[] { "AC", "9H", "KH", "KC", "KD", "KS", "10S", "10D", "9S", "QD", "JS", "10H", "8S", "QH", "JD", "AD", "JC", "AS", "QS", "AH", "JH", "10C", "9C", "8H", "QC", "9D" });
+            Assert.AreEqual("2 26", actual);
         }
 
         [Test]
-        public void HorsesInAnyOrder()
+        public void TwentySixCardsMediumLength()
         {
-            var actual = HorseStrengths.DifferenceBetweenClosest(new[] { 10, 5, 15, 17, 3, 8, 11, 28, 6, 55, 7 });
-            Assert.AreEqual(1, actual);
+            var actual = War.Play(
+                new[] { "6H", "7H", "6C", "QS", "7S", "8D", "6D", "5S", "6S", "QH", "4D", "3S", "7C", "3C", "4S", "5H", "QD", "5C", "3H", "3D", "8C", "4H", "4C", "QC", "5D", "7D" },
+                new[] { "JH", "AH", "KD", "AD", "9C", "2D", "2H", "JC", "10C", "KC", "10D", "JS", "JD", "9D", "9S", "KS", "AS", "KH", "10S", "8S", "2S", "10H", "8H", "AC", "2C", "9H" });
+            Assert.AreEqual("2 56", actual);
+        }
+
+        [Test]
+        public void Battle()
+        {
+            var actual = War.Play(
+                new[] { "8C", "KD", "AH", "QH", "2S" },
+                new[] { "8D", "2D", "3H", "4D", "3S" });
+            Assert.AreEqual("2 1", actual);
         }
     }
 }
--- Katas/Code.cs
+++ Katas/Code.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Text;
 using System.Collections;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 /**
  * Auto-generated code below aims at helping you parse
@@ -14,24 +15,127 @@ class Solution
     static void Main(string[] args)
     {
         Func<int> readInt = () => int.Parse(Console.ReadLine());
-        var horses = new int[readInt()].Select(idx => readInt());
-        Console.WriteLine(HorseStrengths.DifferenceBetweenClosest(horses));
+        Func<int, IList<string>> readDeck = cardCount =>
+            Enumerable.Range(0, cardCount)
+                .Select(i => Console.ReadLine())
+                .ToList();
+
+        var p1Deck = readDeck(readInt());
+        var p2Deck = readDeck(readInt());
+
+        // Write an action using Console.WriteLine()
+        // To debug: Console.Error.WriteLine("Debug messages...");
+
+        var result = War.Play(p1Deck, p2Deck);
+
+        Console.WriteLine(result);
     }
 }
 
-public static class HorseStrengths
+public static class War
 {
-    public static int DifferenceBetweenClosest(IEnumerable<int> horses)
+    public static string Play(IList<string> p1Deck, IList<string> p2Deck)
     {
-        int closest = -1;
-        int previous = -1;
-        foreach (var horse in new List<int>(horses).OrderBy(x => x))
+        var rounds = 0;
+        var decks = new[] {
+            new Queue<Card>(p1Deck.Select(s => new Card(s))),
+            new Queue<Card>(p2Deck.Select(s => new Card(s)))
+        };
+
+        while (decks[0].Any() || decks[1].Any())
         {
-            var difference = horse - previous;
-            closest = closest == -1 ? difference : Math.Min(difference, closest);
-            previous = horse;
+            rounds++;
+
+            // Battle
+            var p1 = decks[0].Dequeue();
+            var p2 = decks[1].Dequeue();
+
+            if (p1 > p2)
+            {
+                decks[0].Enqueue(p1);
+                decks[0].Enqueue(p2);
+            }
+
+            if (p2 > p1)
+            {
+                decks[1].Enqueue(p1);
+                decks[1].Enqueue(p2);
+            }
+
+            if (p2 == p1)
+            {
+                throw new NotImplementedException("WAR!");
+
+                // First, both players place the three next cards of their pile face down.
+                for (int i = 0; i < 3; i++)
+                {
+                    var wp1 = decks[0].Dequeue();
+                    var w2 = decks[1].Dequeue();
+                }
+
+                // Then they go back to step 1 to decide who is going to win the war (several "wars" can be chained).
+
+                // As soon as a player wins a "war", the winner adds all the cards from the "war" to their deck.
+            }
+
+            // Player Won?
+            if (!decks[0].Any()) return $"2 {rounds}";
+            if (!decks[1].Any()) return $"1 {rounds}";
         }
 
-        return closest;
+        return "PAT";
+    }
+}
+
+internal class Card
+{
+    private static Dictionary<string, int> _valueMap = new Dictionary<string, int>
+    {
+        { "2", 1 },
+        { "3", 2 },
+        { "4", 3 },
+        { "5", 4 },
+        { "6", 5 },
+        { "7", 6 },
+        { "8", 7 },
+        { "9", 8 },
+        { "10", 9 },
+        { "J", 10 },
+        { "Q", 11 },
+        { "K", 12 },
+        { "A", 13 },
+    };
+
+    public string Value { get; private set; }
+    public string Suit { get; private set; }
+
+    private int intValue;
+
+    public Card(string value)
+    {
+        var match = Regex.Match(value, @"(?<value>(\d{1,2}|[JQKA]))(?<suit>[HSCD])");
+        Value = match.Groups["value"].Value;
+        intValue = _valueMap[Value];
+        Suit = match.Groups["suit"].Value;
+    }
+
+    public static bool operator >(Card a, Card b)
+    {
+        return a.intValue > b.intValue;
+    }
+
+    public static bool operator <(Card a, Card b)
+    {
+        return a.intValue < b.intValue;
+    }
+
+    public static bool operator ==(Card a, Card b)
+    {
+        return a.intValue == b.intValue;
+    }
+
+    public static bool operator !=(Card a, Card b)
+    {
+        return a.intValue != b.intValue;
     }
 }
