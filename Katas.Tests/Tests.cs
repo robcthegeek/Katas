e981d6197e63037db37e5b253c0e00fefbd28031
Rob Cooper <rob.cooper@thetrainline.com>
2018-02-16 15:26:40 +0000

    Tests Passing

--- Katas.Tests/Tests.cs
+++ Katas.Tests/Tests.cs
@@ -1,14 +1,12 @@
-﻿using NUnit.Framework;
+﻿using System;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Katas.Tests
 {
     [TestFixture]
     public class Tests
     {
-        [Test]
-        public void Test()
-        {
-            Assert.True(Defib.Solve());
-        }
     }
 }
--- Katas/Code.cs
+++ Katas/Code.cs
@@ -1,15 +1,127 @@
-﻿namespace Katas
-{
-    public class Code
-    {
+using System;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
 
+// Don't Panic! https://www.codingame.com/ide/puzzle/don't-panic-episode-1
+
+class Player
+{
+    static void Main(string[] args)
+    {
+        var input = Console.ReadLine();
+        var initState = new InitState(input);
+
+        Enumerable.Range(0, initState.NumElevators)
+            .ToList()
+            .ForEach(i => initState.AddElevator(Console.ReadLine()));
+
+        var panic = new DontPanic(initState);
+
+        while (true)
+        {
+            var inputs = Console.ReadLine().Split(' ');
+            var cloneFloor = inputs.AsInt(0);
+            var clonePos = inputs.AsInt(1);
+            var direction = inputs[2];
+
+            Console.WriteLine(panic.Next(cloneFloor, clonePos, direction));
+        }
     }
 
-    public static class Defib
+    public class DontPanic
     {
-        public static  bool Solve()
+        private readonly InitState state;
+
+        public DontPanic(InitState state)
         {
-            return true;
+            this.state = state;
         }
+
+        public string Next(int cloneFloor, int clonePos, string direction)
+        {
+            if (direction == Direction.None)
+                return Action.Wait;
+
+            var targetPos = state.ExitFloor == cloneFloor
+                ? state.ExitPosition
+                : state.Elevators.Single(x => x.Floor == cloneFloor).Position;
+
+            return GoingRightWay(direction, clonePos, targetPos)
+                ? Action.Wait
+                : Action.Block;
+        }
+
+        private bool GoingRightWay(string direction, int clonePos, int targetPos)
+        {
+            return direction == Direction.Left && targetPos < clonePos ||
+                   direction == Direction.Right && targetPos > clonePos;
+        }
+    }
+}
+
+public class InitState
+{
+    public int Floors { get; set; }
+    public int Width { get; set; }
+    public int Rounds { get; set; }
+    public int ExitFloor { get; set; }
+    public int ExitPosition { get; set; }
+    public int TotalClones { get; set; }
+    public int NumElevators { get; set; }
+    public int AdditionalElevators { get; set; }
+    public List<Elevator> Elevators { get; set; }
+
+    public InitState(string input)
+    {
+        var inputs = input.Split(' ');
+        Floors = inputs.AsInt(0);
+        Width = inputs.AsInt(1);
+        Rounds = inputs.AsInt(2);
+        ExitFloor = inputs.AsInt(3);
+        ExitPosition = inputs.AsInt(4);
+        TotalClones = inputs.AsInt(5);
+        AdditionalElevators = inputs.AsInt(6);
+        NumElevators = inputs.AsInt(7);
+        Elevators = new List<Elevator>();
+    }
+
+    public void AddElevator(string input)
+    {
+        var inputs = input.Split(' ');
+        Elevators.Add(new Elevator
+        {
+            Floor = inputs.AsInt(0),
+            Position = inputs.AsInt(1)
+        });
+    }
+}
+
+public class Elevator
+{
+    public int Floor { get; set; }
+    public int Position { get; set; }
+}
+
+public static class Direction
+{
+    internal const String Left = "LEFT";
+    internal const String Right = "RIGHT";
+    internal const String None = "NONE";
+}
+
+public static class Action
+{
+    internal const string Wait = "WAIT";
+    internal const string Block = "BLOCK";
+}
+
+public static class Extensions
+{
+    internal static int AsInt(this string[] array, int index)
+    {
+        return int.Parse(array[index]);
     }
 }
