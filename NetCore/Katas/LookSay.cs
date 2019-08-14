6e2079e0ca4a468eab335e9128a7d6541e8aa505
Rob Cooper <robc.the.geek@gmail.com>
2019-08-14 06:01:59 +0100

    Hack hack

---
+++ NetCore/Katas/LookSay.cs
@@ -0,0 +1,10 @@
+﻿namespace Katas
+{
+    public static class LookSay
+    {
+        public static string Next(int input)
+        {
+            return null;
+        }
+    }
+}
---
+++ NetCore/Tests/LookSayTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Katas;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class LookSayTests
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.Null(LookSay.Next(0));
+        }
+    }
+}
