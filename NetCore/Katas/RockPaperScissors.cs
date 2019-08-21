0783af2c4f8fea6f1cef915af198b6b89e3721eb
Rob Cooper <robc.the.geek@gmail.com>
2019-08-21 06:27:49 +0100

    First Task Passing

---
+++ NetCore/Katas/RockPaperScissors.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Katas
+{
+    public enum Hand
+    {
+        Rock,
+        Paper,
+        Scissors
+    }
+
+    public static class RockPaperScissors
+    {
+        private static readonly Dictionary<Hand, Hand> _beats = new Dictionary<Hand, Hand>
+        {
+            { Hand.Rock, Hand.Scissors },
+            { Hand.Paper, Hand.Rock },
+            { Hand.Scissors, Hand.Paper },
+        };
+
+        private static bool Beats(this Hand a, Hand b) => _beats[a] == b;
+
+        public static Hand Play(Hand p1, Hand p2)
+        {
+            return p1.Beats(p2) ? p1 : p2;
+        }
+    }
+}
---
+++ NetCore/Tests/RockPaperScissorsTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Katas;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class RockPaperScissorsTests
+    {
+        [TestCase(Hand.Rock, Hand.Scissors)]
+        [TestCase(Hand.Paper, Hand.Rock)]
+        [TestCase(Hand.Scissors, Hand.Paper)]
+        public void Correct_Winner_Returned(Hand winner, Hand loser)
+        {
+            Assert.AreEqual(winner, RockPaperScissors.Play(winner, loser));
+        }
+
+        [Test]
+        public void Determines_Correct_Player()
+        {
+            var p1 = RockPaperScissors.Play(Hand.Scissors, Hand.Paper);
+            var p2 = RockPaperScissors.Play(Hand.Paper, Hand.Scissors);
+
+            Assert.That(p1 == Hand.Scissors && p2 == Hand.Scissors, "Correct winner is not both sides.");
+        }
+
+        [TestCase(Hand.Rock)]
+        [TestCase(Hand.Paper)]
+        [TestCase(Hand.Scissors)]
+        public void Draw_Returns_Same_Hand(Hand hand)
+        {
+            Assert.AreEqual(hand, RockPaperScissors.Play(hand, hand));
+        }
+    }
+}
