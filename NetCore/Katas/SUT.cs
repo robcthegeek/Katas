2a3ca05e91dcc8a751510ab8049e88c683368bde
Rob Cooper <rob.cooper@thetrainline.com>
2019-08-22 13:39:03 +0100

    NetCore - Sparkly Clean

---
+++ NetCore/Katas/SUT.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Katas
+{
+    public static class SUT
+    {
+        public static bool IsThere { get; } = true;
+    }
+}
---
+++ NetCore/Tests/Tests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Katas;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Determines_Correct_Player()
+        {
+            Assert.That(SUT.IsThere);
+        }
+    }
+}
