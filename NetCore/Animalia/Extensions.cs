222efbc2166ec07fcdb0a4a7807da08760cd2832
Rob Cooper <rob.cooper@thetrainline.com>
2019-08-22 17:00:57 +0100

    Add Console App to Play

---
+++ NetCore/Animalia/Extensions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Text;
+
+namespace Animalia
+{
+    internal static class Extensions
+    {
+        internal static Random rng = new Random();
+
+        internal static string Random(this string[] things)
+        {
+            return things[rng.Next(0, things.Length)];
+        }
+
+        internal static void Output(this string value, ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(value);
+            Console.ResetColor();
+        }
+
+        internal static string[] ResourceText(this string file)
+        {
+            var assembly = Assembly.GetEntryAssembly();
+            var names = assembly.GetManifestResourceNames();
+            var resourceStream = assembly.GetManifestResourceStream($"Animalia.{file}");
+            using (var reader = new StreamReader(resourceStream, Encoding.UTF8))
+            {
+                var text = reader.ReadToEnd();
+                return text.Split(Environment.NewLine);
+            }
+        }
+    }
+}
---
+++ NetCore/Animalia/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Katas;
+
+namespace Animalia
+{
+    class Program
+    {
+        private static readonly string[] NiceThings = {
+            "Nice!",
+            "Awesome!",
+            "Yes!",
+            "Great!",
+            "Well done!",
+            "Nailing it!",
+            "Amazing!"
+        };
+
+        static void Main(string[] args)
+        {
+            "Welcome to Animalia!".Output(ConsoleColor.Yellow);
+
+            while (true)
+            {
+                Asciimals.Random.Output(ConsoleColor.Green);
+                Console.WriteLine();
+                "Please name your first animal:".Output(ConsoleColor.Magenta);
+
+                var animalia = new Katas.Animalia("Animals.txt".ResourceText());
+
+                int answers = 0;
+                string input;
+                while ((input = Console.ReadLine()) != string.Empty)
+                {
+                    if ((LowercaseString)input == "q")
+                    {
+                        "Bye!".Output(ConsoleColor.Cyan);
+                        return;
+                    }
+
+                    if (!animalia.Say(input))
+                    {
+                        $"Nope! You've lost after {answers} animal{(answers != 1 ? "s" : "")}!".Output(ConsoleColor.Red);
+                        break;
+                    }
+
+                    answers++;
+                    NiceThings.Random().Output(ConsoleColor.Green);
+                }
+            }
+        }
+    }
+}
