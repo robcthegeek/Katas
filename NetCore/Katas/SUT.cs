106cbbf6f0c114ca4c022940136aa249e6d1466e
Rob Cooper <rob.cooper@thetrainline.com>
2019-08-22 16:04:11 +0100

    Initial Hack

--- NetCore/Katas/SUT.cs
+++ NetCore/Katas/SUT.cs
@@ -1,10 +1,60 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Katas
 {
-    public static class SUT
+    public class LowercaseString
     {
-        public static bool IsThere { get; } = true;
+        private readonly string _value;
+
+        public LowercaseString(string value)
+        {
+            /* People may find this strange, but makes the code cleaner - and also
+             * prevents people from doing things like:
+             *
+             *  var lower = "I LIED"
+             */
+            _value = value?.ToLowerInvariant();
+        }
+
+        public static implicit operator string(LowercaseString lcs) => lcs._value;
+        public static implicit operator LowercaseString(string value) => new LowercaseString(value);
+    }
+
+    public class Animalia
+    {
+        private readonly List<string> _said = new List<string>();
+        private string _previous;
+        private readonly IEnumerable<string> _animalList;
+
+        public Animalia(IEnumerable<string> animalList)
+        {
+            _animalList = animalList?
+                .Select(x => x.ToLowerInvariant())
+                .ToArray();
+        }
+
+        public bool Say(LowercaseString input) {
+            string animal = input;
+
+            if (!_animalList.Contains(animal) || _said.Contains(animal)) return false;
+
+            if (_previous.IsNotSet() || animal.FirstLetter() == _previous.LastLetter())
+            {
+                _said.Add(animal);
+                _previous = input;
+                return true;
+            };
+
+            return false;
+        }
+    }
+
+    internal static class Extensions
+    {
+        internal static char FirstLetter(this string value) => value[0];
+        internal static char LastLetter(this string value) => string.IsNullOrWhiteSpace(value) ? default : value[value.Length - 1];
+        internal static bool IsNotSet(this string value) => string.IsNullOrWhiteSpace(value);
     }
 }
--- NetCore/Tests/Tests.cs
+++ NetCore/Tests/Tests.cs
@@ -9,10 +9,55 @@ namespace Tests
     [TestFixture]
     public class Tests
     {
-        [Test]
-        public void Determines_Correct_Player()
+        private List<string> _animalList;
+        private Animalia _animalia;
+
+        [SetUp]
+        public void SetUp()
         {
-            Assert.That(SUT.IsThere);
+            _animalList = new List<string>() { "pig", "goat", "tiger", "rat" };
+            _animalia = new Animalia(_animalList);
+        }
+
+        [Test]
+        public void Say_WithNoPrevious_OK()
+        {
+            Assert.True(_animalia.Say("pig"));
+        }
+
+        [Test]
+        public void Say_FirstLetterMatchesLastOfPrevious_OK()
+        {
+            _animalia.Say("pig");
+            Assert.True(_animalia.Say("goat"));
+        }
+
+        [Test]
+        public void Say_FirstLetterDoesntMatchLastOfPrevious_NotOK()
+        {
+            _animalia.Say("pig");
+            Assert.False(_animalia.Say("tiger"));
+        }
+
+        [Test]
+        public void Say_PreviouslyUsedAnimal_NotOK()
+        {
+            _animalia.Say("tiger");
+            _animalia.Say("rat");
+            Assert.False(_animalia.Say("tiger"));
+        }
+
+        [Test]
+        public void Say_CaseInsensitive()
+        {
+            _animalia.Say("tiger");
+            Assert.True(_animalia.Say("RAT"));
+        }
+
+        [Test]
+        public void Say_NotAnAnimal_NotOK()
+        {
+            Assert.False(_animalia.Say("car beep beep"));
         }
     }
 }
