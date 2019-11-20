6ffc05ab5d02574194c710a855bb4ea080e6e114
Rob Cooper <robc.the.geek@gmail.com>
2019-11-20 07:18:07 +0000

    Hacky CLI to output story from files

---
+++ TriWriteAStory/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using Katas;
+
+namespace TriWriteAStory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = "";
+            var inputSource = "";
+
+            // path?
+            if (Path.HasExtension(args[0]))
+            {
+                input = File.ReadAllText(args[0]);
+                inputSource = $"file '{args[0]}'";
+            }
+            else
+            {
+                input = args[0];
+                inputSource = $"text: '{args[0]}'";
+            }
+
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                throw new ApplicationException("No text given or path not found.");
+            }
+
+            var story = TrigramStory.From(Trigrams.Find(input));
+
+            const string outputFile = "MyTriStory.txt";
+            File.WriteAllText(outputFile, story);
+
+            Console.WriteLine($"Trigram story created from {inputSource}.");
+            Console.WriteLine($"Saved to '{outputFile}'");
+        }
+    }
+}
