6e736edbc522a26f44df97c8a5a029102b422402
Rob Cooper <rob.cooper@thetrainline.com>
2020-02-05 16:57:38 +0000

    hackhackhack

--- Katas/Diamond.cs
+++ Katas/Diamond.cs
@@ -1,15 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Text;
 
 namespace Katas
 {
     public class Diamond
     {
-        public static Diamond For(char letter)
+        string _asStr = "A";
+
+        private Diamond(char letter)
         {
-            return new Diamond();
+            if (letter == 'A') return;
+
+            int index = letter - 'A';
+
+            var sb = new StringBuilder();
+            int current = letter;
+            for (int i = index; i >= 0; i--)
+            {
+                if (i == 0)
+                {
+                    var pad = (index - 1) / 2;
+                    sb.AppendLine($"{new string(' ', pad)}{(char)current}{new string(' ', pad)}");
+                }
+                else
+                {
+                    sb.AppendLine($"{(char)current}{new string(' ', i)}{(char)current}");
+                }
+
+                current--;
+            }
+
+            _asStr = sb.ToString();
         }
 
-        public static implicit operator string(Diamond diamond) => "A";
+        public static Diamond For(char letter) => new Diamond(letter);
+
+        public static implicit operator string(Diamond diamond) => diamond._asStr;
     }
 }
