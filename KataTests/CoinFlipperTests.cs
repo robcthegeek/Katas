859698a9b8ce246b8e00dc1eb7c3e346102b1d1b
Rob Cooper <rob.cooper@thetrainline.com>
2020-03-03 15:46:00 +0000

    Hacky first pass w/ tests

--- KataTests/CoinFlipperTests.cs
+++ KataTests/CoinFlipperTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using Katas;
 using Xunit;
 
@@ -7,9 +8,27 @@ namespace KataTests
     public class CoinFlipperTests
     {
         [Fact]
-        public void Tests_Are_Working()
+        public void flipper_outputs_101_results()
         {
-            Assert.NotEqual("pending", CoinFlipper.Flip().winner);
+            var result = CoinFlipper.Flip();
+            Assert.Equal(101, result.results.Length);
+        }
+
+        [Fact]
+        public void flipper_outputs_some_heads_and_tails()
+        {
+            // WARN: In theory - this could return all 'Heads', but YOLOOOO!
+            var result = CoinFlipper.Flip();
+            Assert.Contains('H', result.results);
+            Assert.Contains('T', result.results);
+        }
+
+        [Fact]
+        public void flipper_outputs_winner()
+        {
+            var result = CoinFlipper.Flip();
+            var winner = result.results.Count(x => x == 'H') > 50 ? "Heads" : "Tails";
+            Assert.Equal($"{winner} Wins!", result.winner);
         }
     }
 }
--- Katas/CoinFlipper.cs
+++ Katas/CoinFlipper.cs
@@ -1,10 +1,25 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Katas
 {
     public static class CoinFlipper
     {
-        public static (string results, string winner) Flip() => ("pending", "pending");
+        public static (string results, string winner) Flip()
+        {
+            var rng = new Random();
+            var results = new List<char>();
+
+            for (int i = 0; i < 101; i++)
+            {
+                results.Add(rng.Next(0, 2) == 1 ? 'H' : 'T');
+            }
+
+            var winner = results.Count(x => x == 'H') > 50 ? "Heads" : "Tails";
+
+            return (string.Join("", results), $"{winner} Wins!");
+        }
     }
 }
